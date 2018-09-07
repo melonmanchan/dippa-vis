c3680d45099f4498dc23250c2722e66284424522
Matti Jokitulppo <melonmanchan@gmail.com>
2018-09-07 14:33:36 +0300

    Some more work on google emotion vis

--- src/db/index.js
+++ src/db/index.js
@@ -1,5 +1,5 @@
-//       - DATABASE_URL=postgresql://user:pw@db:5432/db?sslmode=disable
 const { Pool } = require('pg')
+
 const pool = new Pool({
   connectionString:
     process.env.DATABASE_URL ||
